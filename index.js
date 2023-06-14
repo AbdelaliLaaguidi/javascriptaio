49d9a104bc88a1aa3e9d2ddc87fd43fcf2276e9f
AbdelaliLaaguidi <abdelali.laaguidi.17@gmail.com>
2023-06-14 11:27:42 +0100

    Learning about for ... of

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ for (let prop in house) {
   divMsg = `${divMsg}<br><li>${prop}: ${house[prop]}</li>`;
   document.getElementById("divMsg").innerHTML = divMsg;
 }
+
+const names = ["Abdelali", "Imad", "Mohamed", "Youssef", "Amine", "Yassine"];
+let divMsg2 = "<h2>Friends list:</h2>";
+for (let name of names) {
+  divMsg2 = `${divMsg2}<br><li>${name}</li>`;
+  document.getElementById("divMsg2").innerHTML = divMsg2;
+}
